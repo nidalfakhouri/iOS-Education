66049988424fba676362b8d756bd5b8ec2f6d0d1
Nidal Fakhouri <>
2018-05-04 16:23:23 -0400

    more on collections

--- 03 - Collections.playground/Contents.swift
+++ 03 - Collections.playground/Contents.swift
@@ -10,21 +10,57 @@ array.append("nidal")
 array.append("john")
 array.append("jane")
 
+print("------------------")
+// Loop option 1
 for i in 0..<array.count {
     let string = array[i]
     print(string)
 }
 
 print("------------------")
+// Loop option 2
+for i in 0...array.count - 1 {
+    let string = array[i]
+    print(string)
+}
 
+print("------------------")
+// Loop option 3 - my most favorite one to use if possible
 for string in array {
     print(string)
 }
 
 print("------------------")
-
+// Loop option 4
 for (index, string) in array.enumerated() {
     print("Item \(index + 1): \(string)")
+}
+
+print("------------------")
+
+// Loop option 5
+array.forEach { print($0) }
+
+print("------------------")
+
+// arrays can be optional also and have optional types!
+var optionalArray: [String?]? = nil
+optionalArray = [String?]()
+
+let string1: String? = nil
+let string2 = "nidal"
+let string3: String? = "foo bar"
+optionalArray?.append(string1)
+optionalArray?.append(string2)
+optionalArray?.append(string3)
+print(optionalArray)
+
+if let nonOptionalArray = optionalArray {
+    for optionalString in nonOptionalArray {
+        if let nonOptionalString = optionalString {
+            print(nonOptionalString)
+        }
+    }
 }
 
 print("------------------")
