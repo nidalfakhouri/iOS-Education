ec059a21d7a2dad83789399d0cd0551666d1c610
Nidal Fakhouri <>
2018-05-17 19:21:36 -0400

    add current location example

--- iOS-Education/Current Location/CurrentLocationExampleViewController.swift
+++ iOS-Education/Current Location/CurrentLocationExampleViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CurrentLocationExampleViewController: UIViewController {
 
+    var locationManager = CLLocationManager()
+    @IBOutlet weak var locationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Current Location"
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.distanceFilter = 50.0
+        
+        // dont forget to add a usage description
+        // https://developer.apple.com/documentation/corelocation/choosing_the_authorization_level_for_location_services/requesting_always_authorization
+    }
+}
+
+extension CurrentLocationExampleViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // most recent location
+        if let location = locations.last {
+            self.locationLabel.text = location.coordinate.string
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
+
+extension CLLocationCoordinate2D {
+    var string: String {
+        return String(format: "%.4f˙, %.4f˙", latitude, longitude)
     }
 }
